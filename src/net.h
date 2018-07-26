140481520e7061eedeb4aa8f108c272eb69f65a8
arnikz <a.kuzniar@esciencecenter.nl>
2018-07-27 01:37:29 +0200

    Update usage info, doc & formatting.

--- src/net.h
+++ src/net.h
@@ -1,54 +1,29 @@
-/* 
- * File:	net.h
- * Author:	Arnold Kuzniar
- * Date:	29-04-2008
- * Version:	1.0
- *
- ***************************************************************************
- * Copyright 2008 Arnold Kuzniar
- *
- * This file is part of netclust/netindex.
- *
- * netclust/netindex is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * netclust/netindex is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser Public License
- * along with netclust/netindex.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef NET_H
 #define	NET_H
-
-#define _FILE_OFFSET_BITS 64	/* large file support (LFS) */
-#define LINE_BUF 100		/* line buffer */
-#define NODE_BUF 30		/* node alias buffer */
-#define OOPS(s) { perror(s); exit(EXIT_FAILURE); }	/* exit with error message */
-#define MALLOC(s, t) if (((s) = malloc(t)) == NULL) OOPS("Error: malloc()\n");	/* allocate memory */
+#define VER "1.1.0"
+#define _FILE_OFFSET_BITS 64 /* large file support (LFS) */
+#define LINE_BUF 100 /* line buffer */
+#define NODE_BUF 30 /* node alias buffer */
+#define OOPS(s) { perror(s); exit(EXIT_FAILURE); } /* exit with error message */
+#define MALLOC(s, t) if (((s) = malloc(t)) == NULL) OOPS("Error: malloc()\n");
 #define FOPEN(fp, fn, fm) if ((fp = fopen(fn, fm)) == NULL) OOPS(fn);
-#define FCLOSE(fp, fn) if (fclose(fp)) OOPS(fn);	/* close file */
+#define FCLOSE(fp, fn) if (fclose(fp)) OOPS(fn);
 
 /* typedefs */
-typedef struct _idx_edge_ IDX_EDGE;	/* indexed graph edge */
-typedef struct _edge_ EDGE;	/* graph edge */
+typedef struct _idx_edge_ IDX_EDGE; /* indexed graph edge */
+typedef struct _edge_ EDGE; /* graph edge */
 
 /* type declarations */
 struct _edge_ {
-    char nodeA[NODE_BUF];
-    char nodeB[NODE_BUF];
-    float weight;
+	char  nodeA[NODE_BUF];
+	char  nodeB[NODE_BUF];
+	float weight;
 };
 
 struct _idx_edge_ {
-    unsigned int nodeA;		/* index node A */
-    unsigned int nodeB;		/* index node B */
-    float weight;		/* edge weight */
+	unsigned int nodeA; /* index node A */
+	unsigned int nodeB; /* index node B */
+	float        weight; /* edge weight */
 };
 
-#endif				/* NET_H */
+#endif /* NET_H */
--- src/netclust_main.c
+++ src/netclust_main.c
@@ -1,63 +1,10 @@
 /*
- * File:	netclust_main.c
- * Author:	Arnold Kuzniar
- * Date:	29-04-2008
- * Version:	1.0
- *
- ****************************************************************************
- * Copyright 2008 Arnold Kuzniar
- *
- * This file is part of netclust/netindex.
- *
- * netclust/netindex is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * netclust/netindex is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser Public License
- * along with netclust/netindex.  If not, see <http://www.gnu.org/licenses/>.
- *
- ****************************************************************************
- *
- * Program description:
- *  netclust is a very efficient graph-based clustering program suitable for
- *  finding single-linkage clusters in very large biological graphs (networks).
- *  The program makes a use of an efficient implementation of the union-find
- *  algorithm (UFA) that requires linear space and has a quasi-linear time
- *  complexity [Tarjan and van Leeuwen, 1984].
- *
- * Usage:
- *  Before the actual clustering process, the user must first index the input
- *  graph using the netindex program. 
- * 
- * Inputs and outputs:
- *  FILE (mandatory) - input file of a (non-indexed) graph
- *  
- *  OUTPUT MODE (mandatory) - output clusters either into STDOUT (O) or into a file (F)
- *      Clusters can be written in two distinct formats:
- *      
- *      O1/F1 - one cluster member per line, the columns are separated by "\t"
- *      
- *         [cluster] [member]
- *
- *      O2/F2 - all cluster members per line, the columns are separated by "\t",
- *      cluster members are separated by " " 
- *
- *         [cluster] [cluster size] [members]
- *  
- *  WEIGHT TYPE - edge weights might refer to either similarity (S) or distance (D) measure (default S)
- *  WEIGHT CUTOFF - cutoff value for edge weights
- *
- *  Note:
- *   The netclust program does not process the input FILE per se, but it uses two binary files
- *   (*.nidx and *.eidx) derived by indexing of the input FILE. Clusters are written into an
- *   output file (*.clst) only if F1/F2 output modes are selected. A log file (*.netindex.log)
- *   is written for each netclust run.
+ * netclust is an efficient graph-based program for detecting single-linkage
+ * clusters in very large biological graphs (networks). The program makes use of
+ * an efficient implementation of the union-find algorithm (UFA) that requires
+ * (nearly) linear time and space complexity (Tarjan and van Leeuwen, 1984).
+ * netclust takes the index graph files (.nidx and .eidx) and outputs clusters
+ * into a text file (.clst) or STDOUT, including a log (.netclust.log).
  */
 
 #include <stdio.h>
@@ -65,35 +12,35 @@
 #include "net.h"
 #include "netclust.h"
 
-int main(int argc, char **argv)
+int 
+main(int argc, char **argv)
 {
-    /* declare variables */
-    PARTITION *par;
-    NCLS_IO_SET ioset, *pt_ioset;
-    NIDX_TABLE table, *pt_table;
+	/* declare variables */
+	PARTITION *par;
+	NCLS_IO_SET	ioset, *pt_ioset;
+	NIDX_TABLE	table, *pt_table;
 
-    pt_ioset = &ioset;
-    pt_table = &table;
+	pt_ioset = &ioset;
+	pt_table = &table;
 
-    /* validate the number of input args */
-    if (argc < 3 || argc > 5)
-	NclsUsage();
+	/* validate the number of input args */
+	if (argc < 2 || argc > 5)
+		NclsUsage();
 
-    /* store IO parameters */
-    ioset = NclsStoreIOset(argc, argv);
+	/* store IO parameters */
+	ioset = NclsStoreIOset(argc, argv);
 
-    /* store node indices into lookup table */
-    StoreNodeIdx(pt_table, pt_ioset);
-    
-    /* retrieve all graph clusters (partition) */
-    par = GetGraphPartition(pt_table, pt_ioset);
+	/* store node indices into lookup table */
+	StoreNodeIdx(pt_table, pt_ioset);
 
-    /* output clusters */
-    OutputGraphPartition(par, pt_ioset);
+	/* retrieve all graph clusters (partition) */
+	par = GetGraphPartition(pt_table, pt_ioset);
 
-    /* write a log file */
-    NclsWriteLogFile(pt_ioset);
+	/* output clusters */
+	OutputGraphPartition(par, pt_ioset);
 
-    exit(EXIT_SUCCESS);
+	/* write a log file */
+	NclsWriteLogFile(pt_ioset);
+
+	exit(EXIT_SUCCESS);
 }
-
